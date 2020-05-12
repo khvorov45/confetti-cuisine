6b5b7b09f60613ada83b33f2217ede6cad253472
Arseniy Khvorov <khvorov45@gmail.com>
2020-05-13 08:26:33 +1000

    add routes to main

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to Confetti Cuisine')
 })
 
+app.get('/courses', homeController.showCourses)
+app.get('/contact', homeController.showSignUp)
+app.post('/contact', homeController.postedSignUpForm)
+
 app.listen(app.get('port'), () => {
   console.log(
     `Server running at http://localhost:${app.get('port')}`
