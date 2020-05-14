36f384eb24ab67df8945410f82d44bf378f56030
Arseniy Khvorov <khvorov45@gmail.com>
2020-05-15 07:34:38 +1000

    style change

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,13 +1,13 @@
-const httpStatus = require('http-status-codes');
+const httpStatus = require('http-status-codes')
 
 exports.pageNotFoundError = (req, res) => {
   res.status(httpStatus.NOT_FOUND)
   res.render('error')
-};
+}
 
 exports.internalServerError = (error, req, res, next) => {
-  let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+  const errorCode = httpStatus.INTERNAL_SERVER_ERROR
   console.log(`ERROR occurred: ${error.stack}`)
-  res.status(errorCode);
-  res.send(`${errorCode} | Sorry, our application is taking a nap!`);
-};
+  res.status(errorCode)
+  res.send(`${errorCode} | Sorry, our application is taking a nap!`)
+}
