5c74eb63646b95fede94c1e1a147f4f595f3d650
Arseniy Khvorov <khvorov45@gmail.com>
2020-05-15 10:30:41 +1000

    add db controller

---
+++ controllers/subscribersController.js
@@ -0,0 +1,9 @@
+const Subscriber = require('../models/subscriber')
+
+exports.getAllSubscribers = (req, res, next) => {
+  Subscriber.find({}, (error, subscribers) => {
+    if (error) next(error)
+    req.data = subscribers
+    next()
+  })
+}
--- main.js
+++ main.js
@@ -6,8 +6,9 @@ const mongoose = require('mongoose')
 
 const homeController = require('./controllers/homeController')
 const errorController = require('./controllers/errorController')
-
-const Subscriber = require('./models/subscriber')
+const subscribersController = require(
+  './controllers/subscribersController'
+)
 
 const dburl = 'mongodb://localhost:27017/recipe_db'
 
@@ -17,38 +18,6 @@ const db = mongoose.connection
 db.once('open', () => {
   console.log('Successfully connected to MongoDB using Mongoose')
 })
-
-var myQuery = Subscriber.findOne({
-  name: 'Jon Wexler'
-})
-  .where('email', /wexler/)
-myQuery.exec((error, data) => {
-  if (error) throw error
-  if (data) console.log(data.name)
-})
-
-/*
-var subscriber1 = new Subscriber({
-  name: 'Jon Wexler',
-  email: 'jon@jonwexler.com'
-})
-
-subscriber1.save((error, savedDocument) => {
-  if (error) console.log(error)
-  console.log(savedDocument)
-})
-
-Subscriber.create(
-  {
-    name: 'Jon Wexler',
-    email: 'jon@jonwexler.com'
-  },
-  function (error, savedDocument) {
-    if (error) console.log(error)
-    console.log(savedDocument)
-  }
-)
-*/
 
 const app = express()
 
@@ -70,6 +39,11 @@ app.get('/', homeController.showHome)
 app.get('/courses', homeController.showCourses)
 app.get('/contact', homeController.showSignUp)
 app.post('/contact', homeController.postedSignUpForm)
+app.get('/subscribers', subscribersController.getAllSubscribers,
+  (req, res, next) => {
+    console.log(req.data)
+    res.render('subscribers', { subscribers: req.data })
+  })
 
 app.use(errorController.pageNotFoundError)
 app.use(errorController.internalServerError)
